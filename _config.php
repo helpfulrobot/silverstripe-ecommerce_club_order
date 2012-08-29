f5f6c96d61c080aec485a35d8fbb309d2fd362b3
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-08-29 22:27:30 +0000

    creating 1.0 branch
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_club_order/branches/1.0@3250 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+*@author Nicolaas [at] sunnysideup.co.nz
+*
+**/
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerce_check_availability MODULE ----------------===================
+//OrderStep::add_order_step_to_include("OrderStep_CheckAvailability", $placeAfter = "OrderStep_Created");
+//===================---------------- END ecommerce_check_availability MODULE ----------------===================
+
